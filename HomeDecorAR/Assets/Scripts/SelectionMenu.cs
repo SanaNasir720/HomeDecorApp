7627c3a5ed37a1814e0f0b2d89dfcfd8446b6400
s8sanasi <s8sanasi@stud.uni-saarland.de>
2020-08-15 16:13:38 +0200

    Final Submission

---
+++ HomeDecorAR/Assets/Scripts/SelectionMenu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectionMenu : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject panel;
+    bool isActive = false;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        if(!isActive)
+        {
+            panel.SetActive(true);
+            isActive = true;
+        }
+        else
+        {
+            panel.SetActive(false);
+            isActive = false;
+        }
+        
+
+    }
+}
---
+++ HomeDecorAR/Assets/Scripts/VaseSelection.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VaseSelection : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject indicator;
+    public GameObject vase;
+    bool isActive = false;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    void TaskOnClick()
+    {
+        indicator.GetComponent<IndicatorBehaviourController>().objectToPlace = vase;
+
+
+    }
+}
